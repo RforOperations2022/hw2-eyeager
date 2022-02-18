5abd05efbdfdd56d3320ae5180320ebc7301c61f
emmayeager <eyeager@andrew.cmu.edu>
2022-02-18 18:28:17 -0500

    Remove unrelated sections

--- BaltimoreEmployment/app.R
+++ BaltimoreEmployment/app.R
@@ -8,12 +8,6 @@ library(tidyverse)
 library(readxl)
 
 # Load and clean data ----------------------------------------------
-starwars.load <- starwars %>%
-    mutate(films = as.character(films),
-           vehicles = as.character(vehicles),
-           starships = as.character(starships),
-           name = as.factor(name))
-
 top.employers <- read_excel('Data/top_employers.xlsx') %>%
     mutate(Rank = as.numeric(Rank),
            Company = as.character(Company),
@@ -44,27 +38,9 @@ sidebar <- dashboardSidebar(
         id = "tabs",
         
         # Menu Items ----------------------------------------------
-        menuItem("Plot", icon = icon("bar-chart"), tabName = "plot"),
-        menuItem("Table", icon = icon("table"), tabName = "table", badgeLabel = "new", badgeColor = "green"),
         menuItem("Top Employers", tabName = "employers"),
-        menuItem("Wages by Occupation", tabName = "wages"),
         menuItem("Top Industries", tabName = "industry"),
-        
-        # Inputs: select variables to plot ----------------------------------------------
-        selectInput("worldSelect",
-                    "Homeworld:",
-                    choices = sort(unique(starwars.load$homeworld)),
-                    multiple = TRUE,
-                    selectize = TRUE,
-                    selected = c("Naboo", "Tatooine")),
-        
-        # Birth year Selection ----------------------------------------------
-        sliderInput("birthSelect",
-                    "Birth Year:",
-                    min = min(starwars.load$birth_year, na.rm = T),
-                    max = max(starwars.load$birth_year, na.rm = T),
-                    value = c(min(starwars.load$birth_year, na.rm = T), max(starwars.load$birth_year, na.rm = T)),
-                    step = 1),
+        menuItem("Wages by Occupation", tabName = "wages"),
         
         # Number of employers selection ----------------------------------------------
         sliderInput("employerSelect",
@@ -74,7 +50,7 @@ sidebar <- dashboardSidebar(
                     value = c(1,30),
                     step = 1),
         
-        # Show public sector
+        # Show public sector selection
         checkboxInput(inputId = "show_public",
                       label = "Show public sector",
                       value = TRUE),
@@ -90,32 +66,9 @@ sidebar <- dashboardSidebar(
 # Dashboard body ----------------------------------------------
 body <- dashboardBody(tabItems(
     
-    # Plot page ----------------------------------------------
-    tabItem("plot",
-            
-            # Input and Value Boxes ----------------------------------------------
-            fluidRow(
-                infoBoxOutput("mass"),
-                valueBoxOutput("height")
-            ),
-            
-            # Plot ----------------------------------------------
-            fluidRow(
-                tabBox(title = "Plot",
-                       width = 12,
-                       tabPanel("Mass", plotlyOutput("plot_mass")),
-                       tabPanel("Height", plotlyOutput("plot_height")))
-            )
-    ),
-    
-    # Data Table Page ----------------------------------------------
-    tabItem("table",
-            fluidPage(
-                box(title = "Selected Character Stats", DT::dataTableOutput("table"), width = 12))
-    ),
-    
     # Employers page ----------------------------------------------
     tabItem("employers",
+            
             # Input and Value Boxes ----------------------------------------------
             fluidRow(
                 valueBoxOutput("jobs"),
@@ -123,6 +76,7 @@ body <- dashboardBody(tabItems(
                 valueBoxOutput("establishments")
             ),
             
+            # Plot and data ----------------------------------------------------
             fluidRow(
                 tabBox(title = "Employers",
                        width = 12,
@@ -155,30 +109,14 @@ ui <- dashboardPage(header, sidebar, body)
 
 # Define server function required to create plots and value boxes -----
 server <- function(input, output) {
-    
-    # Reactive data function -------------------------------------------
-    swInput <- reactive({
-        starwars <- starwars.load %>%
-            
-            # Slider Filter ----------------------------------------------
-        filter(birth_year >= input$birthSelect[1] & birth_year <= input$birthSelect[2])
-        
-        # Homeworld Filter ----------------------------------------------
-        if (length(input$worldSelect) > 0 ) {
-            starwars <- subset(starwars, homeworld %in% input$worldSelect)
-        }
-        
-        # Return dataframe ----------------------------------------------
-        return(starwars)
-    })
-    
+
     # Reactive top employer function -------------------------------------------
     top.employers.imp <- reactive({
         top.employers[which(top.employers$Rank >= input$employerSelect[1] & 
                              top.employers$Rank <= input$employerSelect[2]),]
     })
     
-    # Create a new column to update colors on scatter plot based on which occupation selected
+    # Reactive selected occupation column
     wages.imp <- reactive({
         wages %>%
             add_column(z = wages$Occupation == input$occ)
@@ -193,20 +131,6 @@ server <- function(input, output) {
         else {
             industry
         }
-    })
-    
-    # Reactive melted data ----------------------------------------------
-    mwInput <- reactive({
-        swInput() %>%
-            melt(id = "name")
-    })
-    
-    # A plot showing the mass of characters -----------------------------
-    output$plot_mass <- renderPlotly({
-        dat <- subset(mwInput(), variable == "mass")
-        
-        # Generate Plot ----------------------------------------------
-        ggplot(data = dat, aes(x = name, y = as.numeric(value), fill = name)) + geom_bar(stat = "identity")
     })
     
     # A plot showing top employers -----------------------------
@@ -241,17 +165,6 @@ server <- function(input, output) {
             ggtitle('Top Industries')
     })
     
-    # A plot showing the height of characters -----------------------------------
-    output$plot_height <- renderPlotly({
-        dat <- subset(mwInput(),  variable == "height")
-        ggplot(data = dat, aes(x = name, y = as.numeric(value), fill = name)) + geom_bar(stat = "identity")
-    })
-    
-    # Data table of characters ----------------------------------------------
-    output$table <- DT::renderDataTable({
-        subset(swInput(), select = c(name, height, mass, birth_year, homeworld, species, films))
-    })
-    
     # Data table of employers ----------------------------------------------
     output$empTable <- DT::renderDataTable({
         top.employers.imp()
@@ -262,25 +175,9 @@ server <- function(input, output) {
         wages
     })
     
-    # Data table of employers ----------------------------------------------
+    # Data table of industries ----------------------------------------------
     output$industryTable <- DT::renderDataTable({
         industry.imp()
-    })
-    
-    # Mass mean info box ----------------------------------------------
-    output$mass <- renderInfoBox({
-        sw <- swInput()
-        num <- round(mean(sw$mass, na.rm = T), 2)
-        
-        infoBox("Avg Mass", value = num, subtitle = paste(nrow(sw), "characters"), icon = icon("balance-scale"), color = "purple")
-    })
-    
-    # Height mean value box ----------------------------------------------
-    output$height <- renderValueBox({
-        sw <- swInput()
-        num <- round(mean(sw$height, na.rm = T), 2)
-        
-        valueBox(subtitle = "Avg Height", value = num, icon = icon("sort-numeric-asc"), color = "blue")
     })
     
     # Total jobs value box ----------------------------------------------
